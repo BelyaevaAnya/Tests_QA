67904c97f8c416609f418e2c6db8b6be27a0cdcc
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-10-03 12:50:01 +0300

    comment

--- HumanMoveUnittestCreate/suite_12_3.py
+++ HumanMoveUnittestCreate/suite_12_3.py
@@ -10,3 +10,16 @@ RunnerToutnamentST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_
 
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(RunnerToutnamentST)
+#
+# test_challange (tests_12_3.RunnerTest.test_challange) ... ok
+# test_false_run (tests_12_3.RunnerTest.test_false_run) ... FAIL
+# test_run (tests_12_3.RunnerTest.test_run) ... ok
+# test_walk (tests_12_3.RunnerTest.test_walk) ... ok
+# test_race_all (tests_12_3.TournamentTest.test_race_all) ... skipped 'Тесты в этом кейсе заморожены'
+# test_race_andrey_and_nick (tests_12_3.TournamentTest.test_race_andrey_and_nick) ... skipped 'Тесты в этом кейсе заморожены'
+# test_race_usain_and_nick (tests_12_3.TournamentTest.test_race_usain_and_nick) ... skipped 'Тесты в этом кейсе заморожены'
+#
+# ======================================================================
+# Тест заведомо ложный
+# FAIL: test_false_run (tests_12_3.RunnerTest.test_false_run)
+# ----------------------------------------------------------------------
